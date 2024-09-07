b42ca3762915cf1f2703d004b6c868e53cf7bd73
ahmedaminebouchmal <bouchmalaabesp@gmail.com>
2024-09-07 19:24:45 +0200

    third cnfs

---
+++ data/index.ts
@@ -0,0 +1,266 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+//https://master.dq2gx305ul3xt.amplifyapp.com////     ////ui.earth.com///
+
+export const projects = [
+  {
+    id: 1,
+    title: "Inventar - Inventory Management Dashboard",
+    des: "Complete Full Stack Inventory Management Dashboard Application using Nextjs, Redux, Node, and AWS.",
+    img: "/p4.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    vercelLink: "https://master.dq2gx305ul3xt.amplifyapp.com",
+    githubLink: "https://github.com/ahmedaminebouchmal/inventar",
+  },
+  {
+    id: 2,
+    title: "Anruf - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Anruf. Seamlessly connect with colleagues and friends.",
+    img: "/anruf.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    vercelLink: "https://anruf.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/anruf",
+  },
+  {
+    id: 3,
+    title: "Feedback Collector - SaaS Application",
+    des: "A REAL Software-as-a-Service app with API features and a payments and credits system using the latest tech stack.",
+    img: "/saas.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    vercelLink: "https://saas-feed.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/Saas-Feed",
+  },
+  {
+    id: 4,
+    title: "Klinik-Verwalter - Appointment System Manager",
+    des: "Manage appointments efficiently with this robust system built using Next.js and Twilio.",
+    img: "/klinik.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://klinik-verwalter.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/KlinikVerwalter",
+  },
+  {
+    id: 5,
+    title: "REISE-APP - a Landing page for Travel Website",
+    des: "a Fully Responsive Modern UI/UX Website | React.js, Next.js 13, Tailwind CSS.",
+    img: "/reise.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://reise-app.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/reise-app",
+  },
+  {
+    id: 6,
+    title: "FIZZI - a 3D Ecommerce Landing Page",
+    des: "a scroll animated 3D landing page with Next.js 14, GSAP, Prismic, Three.js, Tailwind, and TypeScript.",
+    img: "/fizzi.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://fizzi-eta.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/fizzi",
+  },
+  {
+    id: 7,
+    title: "Software-Tool- a Landing Page for SaaS Software Tool",
+    des: "Fully Responsive Landing Page: React + Tailwind CSS.",
+    img: "/tool.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://amazonfordigital.com/",
+    githubLink: "https://github.com/ahmedaminebouchmal/Software-Tool",
+  },
+  {
+    id: 8,
+    title: "FRACTAL - a Frontend Ecommerce App with Nextjs",
+    des: " Next.js app featuring SSR, SSG, responsive design, TailwindCSS, RESTful APIs, Git, Jest, and Cypress testing.",
+    img: "/store.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://fractal-jade.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/fractal",
+  },
+  {
+    id: 8,
+    title: "Animated Apple iPhone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects.",
+    img: "/iphone.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    vercelLink: "https://iphone-three-omega.vercel.app/",
+    githubLink: "https://github.com/ahmedaminebouchmal/iphone",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Amine was a great experience. His expertise in full stack development with Next.js, professionalism, and dedication were apparent throughout our project. Amine's ability to seamlessly integrate different technologies made a significant impact. Highly recommend him for any complex development tasks.",
+    name: "Fatima Zahra",
+    title: "Project Manager at MoroccanTech Solutions",
+  },
+  {
+    quote:
+      "Amine's work on our project was exceptional. His skill with Next.js and his commitment to delivering high-quality results were evident at every stage. He effectively managed both frontend and backend tasks, ensuring smooth project execution. A top-notch developer.",
+    name: "Jessica Smith",
+    title: "Head of Development at American Innovations",
+  },
+  {
+    quote:
+      "Collaborating with Amine was fantastic. His expertise in Next.js and full stack development helped us achieve our project goals efficiently. His professionalism and ability to communicate across teams made the process seamless. He is a valuable asset to any development team.",
+    name: "Carlos Gomez",
+    title: "Lead Developer at Spanish Solutions Inc.",
+  },
+  {
+    quote:
+      "Amine brought a high level of expertise and dedication to our project. His work with Next.js and full stack development was impressive, and his ability to collaborate effectively with international teams was invaluable. Highly recommended for his technical skills and professionalism.",
+    name: "Ahmed Elmansouri",
+    title: "Technical Lead at Moroccan Innovations",
+  },
+  {
+    quote:
+      "Amine's full stack development skills and proficiency with Next.js were crucial to the success of our project. His work ethic, responsiveness, and technical acumen made him a pleasure to work with. I highly recommend him for any complex web development needs.",
+    name: "Sofia Martínez",
+    title: "CTO at Spanish Web Solutions",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/aws-2.svg",
+    nameImg: "/aws.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev ",
+    desc: "Designed and developed mobile app  Android platforms using Java and Kotlin.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ahmedaminebouchmal",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/ahmed-amine-bouchmal-55a3b4287/",
+  },
+];
